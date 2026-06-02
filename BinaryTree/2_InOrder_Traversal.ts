2f7c28eeb2769b90dc22914ef66ef4c142d53ca3
Krunal <kunalrkathiriya123@gmail.com>
2026-06-02 09:12:53 -0500

    Add implementation for inorder traversal of a binary tree

---
+++ BinaryTree/2_InOrder_Traversal.ts
@@ -0,0 +1,29 @@
+function inorderTraversal(root: TreeNode | null): number[] {
+
+    let arrOut: Array<number> = []
+    let stack: TreeNode[] = []
+
+    // Step 1: return [] if node is null
+    if (root === null) {
+        return arrOut
+    }
+
+    // Step 2:curr pointer will point to the  root element 
+    let curr: TreeNode = root
+
+    // Step 3: while the stack is not empty or curr !== null process each node and push it in the arrOut
+    while (curr !== null || stack.length > 0) {
+        while (curr !== null) {
+            stack.push(curr)
+            curr = curr.left as TreeNode
+        }
+
+        curr = stack.pop() as TreeNode;
+        arrOut.push(curr.val)
+        curr = curr.right as TreeNode;
+
+
+    }
+
+    return arrOut
+};
